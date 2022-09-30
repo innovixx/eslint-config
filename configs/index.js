7ce2b41f8f0813eeb32740995e3abd7fcc37d1f2
Kalon Robson <kalon.robson@innovixx.co.uk>
2022-09-30 18:12:01 +0100

    feat: jest and react optional

--- configs/index.js
+++ configs/index.js
@@ -1,8 +1,20 @@
+const path = require('path');
+const { existsSync } = require('fs');
+
+const extensions = [];
+
+if (existsSync(path.resolve(__dirname, '../../jest'))) {
+  extensions.push('jest');
+}
+
+if (existsSync(path.resolve(__dirname, '../../react'))) {
+  extensions.push('react');
+}
+
 module.exports = {
   extends: [
     './base',
-    './jest',
-    './react',
+    ...extensions,
   ].map(require.resolve),
   rules: {},
 };
