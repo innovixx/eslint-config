5b7a918755d9aaf5ae98139e4a8a2ac8f9df25ea
Aman Bhogal <aman.bhogal@innovixx.co.uk>
2025-01-30 16:07:45 +0000

    chore: enhance TypeScript configuration with ECMAScript 2020 support and JSX parsing

--- configs/typescript/rules/typescript.js
+++ configs/typescript/rules/typescript.js
@@ -1,9 +1,20 @@
 module.exports = {
+  languageOptions: {
+    ecmaVersion: 2020,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    sourceType: 'module',
+  },
   settings: {
     'import/resolver': {
       node: {
-        extensions: ['.mjs', '.js', '.ts', '.ts', '.tsx', '.json'],
+        extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx', '.json'],
       },
+      typescript: {},
     },
   },
 };
