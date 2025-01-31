af1680e450f233ceed26d825faf64b25c3169a77
Aman Bhogal <aman.bhogal@innovixx.co.uk>
2025-01-31 09:39:21 +0000

    chore: update ESLint configuration to include recommended rules from @eslint/js

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,14 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { FlatCompat } from '@eslint/eslintrc';
 import tsParser from '@typescript-eslint/parser';
+import js from '@eslint/js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
+  allConfig: js.configs.all,
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
 });
 
 export default [...compat.extends('.'), {
