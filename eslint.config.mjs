453570d12d819f93d318c9a60452e148c26a5e84
Aman Bhogal <aman.bhogal@innovixx.co.uk>
2025-01-31 09:54:41 +0000

    chore: add import resolver settings for ESLint to support TS

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -24,4 +24,12 @@ export default [...compat.extends('.'), {
     },
     sourceType: 'module',
   },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx', '.json'],
+      },
+      typescript: {},
+    },
+  },
 }];
