f62c4576b3531932ab55e1ea539bfc874eb308cd
Aman Bhogal <aman.bhogal@innovixx.co.uk>
2025-02-25 09:09:18 +0000

    chore: update typescript rules

--- config/configs/typescript/rules/all.mjs
+++ config/configs/typescript/rules/all.mjs
@@ -3,10 +3,6 @@
 const variableRules = {
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'warn',
-    '@typescript-eslint/require-await': 'warn',
-    '@typescript-eslint/no-floating-promises': 'warn',
-    '@typescript-eslint/strict-boolean-expressions': 'warn',
-    '@typescript-eslint/no-unsafe-assignment': 'warn',
     '@typescript-eslint/prefer-nullish-coalescing': 'warn',
     '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-unused-vars': [
